af947d5bd33a9e25109e3c0d1b62025d68bdd31a
ChronoAnomaly <chronoanomaly@gmail.com>
2017-05-15 13:08:36 -0400

    Inital version

--- js/server.js
+++ js/server.js
@@ -11,7 +11,7 @@ http.createServer(function(req, resp) {
   var respContent = [];
   var respString = '';
   var endxml = '';
-  var buildxml ='';
+  var buildxml = '';
 
   req.on('data', function(data) {
     respContent.push(data.toString()); //data is a buffer instance
@@ -85,12 +85,41 @@ http.createServer(function(req, resp) {
     console.log(inspect(builder.buildObject(respString)));
 
     request.post({
-      url: 'http://www.customerdatabase.dev/index.php?/public/PublicData/findInformation',
+      url: 'https://www.pictureperfectohio.com/customerdatabase//index.php?/public/PublicData/findInformation',
       form: {
         phone_number: endxml['PolycomIPPhone']['IncomingCallEvent'][0]['CallingPartyNumber'][0]
       }
     }, function(err, httpResponse, body) {
       console.log(body);
+      var info = JSON.parse(body);
+      if (jQuery.isEmptyObject(info)) {
+        $("#ohio-status").text("No client information found!").css("color", "red");
+        //$(".caller-information").hide();
+      } else {
+        $("#ohio-status").text("Client information found!").css("color", "green");
+        //$(".caller-information").show();
+        parseInfo(info);
+      }
+      //console.log(info[0].lead_id);
+    });
+
+    request.post({
+      url: 'https://www.denverphotoboothrentals.com/customerdatabase//index.php?/public/PublicData/findInformation',
+      form: {
+        phone_number: endxml['PolycomIPPhone']['IncomingCallEvent'][0]['CallingPartyNumber'][0]
+      }
+    }, function(err, httpResponse, body) {
+      console.log(body);
+      var info = JSON.parse(body);
+      if (jQuery.isEmptyObject(info)) {
+        $("#colorado-status").text("No client information found!").css("color", "red");
+        $(".caller-information").hide();
+      } else {
+        $("#colorado-status").text("Client information found!").css("color", "green");
+        $(".caller-information").show();
+        parseInfo(info);
+      }
+      //console.log(info[0].lead_id);
     });
 
     //request.post('http://www.customerdatabase.dev/index.php?/public/PublicData/findInformation').form({phone_number: endxml['PolycomIPPhone']['IncomingCallEvent'][0]['CallingPartyNumber']
@@ -114,3 +143,19 @@ http.createServer(function(req, resp) {
 
   resp.end();
 }).listen(8080);
+
+var parseInfo = function(info) {
+  //var info = JSON.parse(body);
+  console.log(info[0].lead_id);
+  var event_datetime = info[0].event_datetime.split(" ");
+  $("#clientName").text(info[0].lead_first_name + " " + info[0].lead_last_name);
+  $("#email").text(info[0].lead_email);
+  $("leadStatus").text(info[0].lead_status);
+  $("#leadSource").text(info[0].lead_source);
+  $("#venueName").text(info[0].venue_name);
+  $("#venueAddress").text(info[0].venue_street_address + " " + info[0].venue_city + ", " + info[0].venue_state + " " + info[0].venue_zip);
+  $("#eventTime").text(event_datetime[1]);
+  $("#eventDate").text(event_datetime[0]);
+  $("#eventLength").text(info[0].event_length);
+  $("#notes").text(info[0].lead_notes);
+};
--- main.js
+++ main.js
@@ -18,7 +18,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 900, height: 600});
+    mainWindow = new BrowserWindow({width: 1000, height: 800});
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/infopage.html');
