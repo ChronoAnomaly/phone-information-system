41efd17969a4e1979bf5ddc944506d1a45636c32
ChronoAnomaly <chronoanomaly@gmail.com>
2017-05-18 12:07:20 -0400

    Updates to match db response

--- js/server.js
+++ js/server.js
@@ -16,73 +16,92 @@ http.createServer(function(req, resp) {
   req.on('data', function(data) {
     respContent.push(data.toString()); //data is a buffer instance
     respString += data.toString();
-    console.log("Hit data");
-    console.log("=========================================================================");
-    console.log("data: " + data.toString());
-    console.log("=========================================================================");
+    // console.log("Hit data");
+    // console.log("=========================================================================");
+    // console.log("data: " + data.toString());
+    // console.log("=========================================================================");
   });
   req.on('end', function() {
-    console.log("end function");
-    console.log("=========================================================================");
-    console.log(respContent);
-    console.log();
-    console.log(respString);
-    console.log();
-
-    //endxml = builder.buildObject(respContent);
-
-    console.log("endxml");
-    console.log("=========================================================================");
-    console.dir(endxml);
-    var xml = "<root>Hello xml2js!</root>"
+    // console.log("end function");
+    // console.log("=========================================================================");
+    // console.log(respContent);
+    // console.log();
+    // console.log(respString);
+    // console.log();
+    //
+    // //endxml = builder.buildObject(respContent);
+    //
+    // console.log("endxml");
+    // console.log("=========================================================================");
+    // console.dir(endxml);
+    // var xml = "<root>Hello xml2js!</root>"
     var value = {};
-    parseString(xml, function(err, result) {
-      console.log("xml test node");
-      console.log("=========================================================================");
-      console.dir(result);
-      console.log();
-    });
+    // parseString(xml, function(err, result) {
+    //   console.log("xml test node");
+    //   console.log("=========================================================================");
+    //   console.dir(result);
+    //   console.log();
+    // });
+    // parseString(respContent, function(err, result) {
+    //   console.log("JSON:");
+    //   console.log("=========================================================================");
+    //   console.dir(JSON.stringify(result));
+    //   console.log();
+    // });
     parseString(respContent, function(err, result) {
-      console.log("JSON:");
-      console.log("=========================================================================");
-      console.dir(JSON.stringify(result));
-      console.log();
-    });
-    parseString(respContent, function(err, result) {
-      console.log("XML:");
-      console.log("=========================================================================");
-      console.dir(result);
+      // console.log("XML:");
+      // console.log("=========================================================================");
+      // console.dir(result);
       endxml = result;
-      console.log();
+      // console.log();
     });
-    parseString(respString, function(err, result) {
-      console.log("String type:");
-      console.log("=========================================================================");
-      console.dir(result);
-      console.log();
-    });
-    parseString(respString, function(err, result) {
-      console.log("String type JSON:");
-      console.log("=========================================================================");
-      console.dir(JSON.stringify(result));
-      //value = JSON.parse(result);
-      console.log();
-    });
-    console.log("respContent");
-    console.log("=========================================================================");
-    console.log(respContent);
-    console.log();
-    console.log("respString");
-    console.log("=========================================================================");
-    console.log(respString);
-    console.log();
-    console.log("Trying to access the value now");
-    console.log("=========================================================================");
-    console.dir(endxml);
-    console.log();
-    console.dir(value);
-    console.log(inspect(endxml))
-    console.log(inspect(builder.buildObject(respString)));
+    // parseString(respString, function(err, result) {
+    //   console.log("String type:");
+    //   console.log("=========================================================================");
+    //   console.dir(result);
+    //   console.log();
+    // });
+    // parseString(respString, function(err, result) {
+    //   console.log("String type JSON:");
+    //   console.log("=========================================================================");
+    //   console.dir(JSON.stringify(result));
+    //   //value = JSON.parse(result);
+    //   console.log();
+    // });
+    // console.log("respContent");
+    // console.log("=========================================================================");
+    // console.log(respContent);
+    // console.log();
+    // console.log("respString");
+    // console.log("=========================================================================");
+    // console.log(respString);
+    // console.log();
+    // console.log("Trying to access the value now");
+    // console.log("=========================================================================");
+    // console.dir(endxml);
+    // console.log();
+    // console.dir(value);
+    // console.log(inspect(endxml))
+    // console.log(inspect(builder.buildObject(respString)));
+
+    // request.post({
+    //   url: 'http://www.customerdatabase.dev/index.php?/public/PublicData/findInformation',
+    //   form: {
+    //     phone_number: endxml['PolycomIPPhone']['IncomingCallEvent'][0]['CallingPartyNumber'][0]
+    //   }
+    // }, function(err, httpResponse, body) {
+    //   console.log(body);
+    //   var info = JSON.parse(body);
+    //   if (jQuery.isEmptyObject(info)) {
+    //     $("#ohio-status").text("No client information found!").css("color", "red");
+    //     //$(".lead-information").hide();
+    //   } else {
+    //     $("#ohio-status").text("Client information found!").css("color", "green");
+    //     $(".lead-information").show();
+    //     parseInfo(info);
+    //   }
+    //   //console.log(info.lead_id);
+    // });
 
     request.post({
       url: 'http://www.customerdatabase.dev/index.php?/public/PublicData/findInformation',
@@ -94,14 +113,14 @@ http.createServer(function(req, resp) {
       var info = JSON.parse(body);
       if (jQuery.isEmptyObject(info)) {
         $("#ohio-status").text("No client information found!").css("color", "red");
-        //$(".caller-information").hide();
+        //$(".lead-information").hide();
       } else {
         $("#ohio-status").text("Client information found!").css("color", "green");
         $(".no-info").hide();
-        $(".caller-information").show();
+        $(".lead-information").show();
         parseInfo(info);
       }
-      //console.log(info[0].lead_id);
+      //console.log(info.lead_id);
     });
 
     request.post({
@@ -114,14 +133,14 @@ http.createServer(function(req, resp) {
       var info = JSON.parse(body);
       if (jQuery.isEmptyObject(info)) {
         $("#colorado-status").text("No client information found!").css("color", "red");
-        // $(".caller-information").hide();
+        // $(".lead-information").hide();
       } else {
         $("#colorado-status").text("Client information found!").css("color", "green");
         $(".no-info").hide();
-        $(".caller-information").show();
+        $(".lead-information").show();
         parseInfo(info);
       }
-      //console.log(info[0].lead_id);
+      //console.log(info.lead_id);
     });
 
     //request.post('http://www.customerdatabase.dev/index.php?/public/PublicData/findInformation').form({phone_number: endxml['PolycomIPPhone']['IncomingCallEvent'][0]['CallingPartyNumber']
@@ -129,14 +148,14 @@ http.createServer(function(req, resp) {
     //      console.log('HEY');
     //  });
 
-    console.log(endxml['PolycomIPPhone']['IncomingCallEvent'][0]['CallingPartyNumber'][0]);
-    console.log();
-    console.log(respContent['CallingPartyNumber']);
-    console.log();
-    console.log(respString.CallingPartyNumber);
-    console.log();
-    console.log(respContent['CallingPartyNumber']);
-    console.log();
+    // console.log(endxml['PolycomIPPhone']['IncomingCallEvent'][0]['CallingPartyNumber'][0]);
+    // console.log();
+    // console.log(respContent['CallingPartyNumber']);
+    // console.log();
+    // console.log(respString.CallingPartyNumber);
+    // console.log();
+    // console.log(respContent['CallingPartyNumber']);
+    // console.log();
 
   });
   resp.writeHead(200, {"Content-Type": "text/plain"});
@@ -148,16 +167,17 @@ http.createServer(function(req, resp) {
 
 var parseInfo = function(info) {
   //var info = JSON.parse(body);
-  console.log(info[0].lead_id);
-  var event_datetime = info[0].event_datetime.split(" ");
-  $("#clientName").text(info[0].lead_first_name + " " + info[0].lead_last_name);
-  $("#email").text(info[0].lead_email);
-  $("leadStatus").text(info[0].lead_status);
-  $("#leadSource").text(info[0].lead_source);
-  $("#venueName").text(info[0].venue_name);
-  $("#venueAddress").text(info[0].venue_address );
-  $("#eventTime").text(info[0].event_time);
-  $("#eventDate").text(info[0].event_date);
-  $("#eventLength").text(info[0].event_length);
-  $("#notes").text(info[0].lead_notes);
+  // console.log('oeprghrseuioghuiovg');
+  // console.log(info.lead_status);
+  // var event_datetime = info.event_datetime.split(" ");
+  $("#clientName").text(info.lead_first_name + " " + info.lead_last_name);
+  $("#email").text(info.lead_email);
+  $("#leadStatus").text(info.lead_status);
+  $("#leadSource").text(info.lead_source);
+  $("#venueName").text(info.venue_name);
+  $("#venueAddress").text(info.venue_address );
+  $("#eventTime").text(info.event_time);
+  $("#eventDate").text(info.event_date);
+  $("#eventLength").text(info.event_length);
+  $("#notes").text(info.lead_notes);
 };
